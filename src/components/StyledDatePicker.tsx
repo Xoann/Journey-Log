b448d5b9df7771a54cffd48b5013c7a9c7723b9d
Liam <liammaguire@outlook.com>
2024-10-25 13:08:17 -0400

    chore: finished styling the date picker

--- src/components/StyledDatePicker.tsx
+++ src/components/StyledDatePicker.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Dayjs } from "dayjs";
 import TextField, { TextFieldProps } from "@mui/material/TextField";
 import {
@@ -19,7 +19,104 @@ const StyledDatePicker: React.FC<StyledDatePickerProps> = ({
   onChange,
 }) => {
   const { currentTheme } = useTheme();
-  const selectedColor = currentTheme.getShade(Shade.High);
+  const [selectedColor, setSelectedColor] = useState<string>(
+    currentTheme.getShade(Shade.High)
+  );
+
+  useEffect(() => {
+    setSelectedColor(currentTheme.getShade(Shade.High));
+  }, [currentTheme]);
+
+  const popperSx = {
+    "& .MuiPaper-root": {
+      backgroundColor: "#111", // Change calendar background color here
+      border: "1px solid white", // Change calendar border color here
+      color: "white", // Change calendar text color here
+      marginTop: "8px",
+      marginBottom: "8px",
+    },
+    "& .MuiPickersDay-root": {
+      color: "white", // Change day text color here
+    },
+    "& .MuiPickersDay-today": {
+      borderColor: "white", // Change today's date border color here
+    },
+    "& .MuiPickersDay-root.Mui-selected": {
+      backgroundColor: `${selectedColor} !important`, // Change selected day background color here
+      color: "white", // Ensure text color is readable
+    },
+    "& .MuiPickersMonth-monthButton.Mui-selected": {
+      backgroundColor: `${selectedColor} !important`,
+    },
+    "& .MuiPickersMonth-monthButton:hover": {
+      border: `1px solid ${selectedColor} !important`,
+    },
+    "& .MuiPickersDay-root.MuiPickersDay-dayOutsideMonth": {
+      // Change days outside current month color here
+      color: "gray",
+    },
+    "& .MuiPickersDay-root:hover": {
+      border: `1px solid ${selectedColor} !important`, // Change selected day border color on hover here
+      color: "white", // Ensure text color is readable
+    },
+    "& .MuiSvgIcon-root": {
+      color: "white", // Change icon color here
+    },
+    "& .MuiTypography-root": {
+      color: "white", // Change text color here
+    },
+  };
+
+  const textFieldSx = {
+    "& .MuiInputLabel-root": {
+      color: "white", // Change label color here
+      "&.Mui-focused": {
+        color: `${selectedColor} !important`, // Change label color when focused here
+      },
+    },
+    "& .MuiInputBase-input": {
+      color: "white", // Change text color here
+    },
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "white", // Change border color here
+      },
+      "&:hover fieldset": {
+        borderColor: "white", // Change border color on hover here
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: "white", // Change border color when focused here
+      },
+    },
+    "& .MuiSvgIcon-root": {
+      // Change icon color here
+      color: "white",
+    },
+  };
+
+  // const textFieldSx = {
+  //   "& .MuiInputLabel-root": {
+  //     color: "white", // Change label color here
+  //   },
+  //   "& .MuiInputBase-input": {
+  //     color: "white", // Change text color here
+  //   },
+  //   "& .MuiOutlinedInput-root": {
+  //     "& fieldset": {
+  //       borderColor: "white", // Change border color here
+  //     },
+  //     "&:hover fieldset": {
+  //       borderColor: "white", // Change border color on hover here
+  //     },
+  //     "&.Mui-focused fieldset": {
+  //       borderColor: "white", // Change border color when focused here
+  //     },
+  //   },
+  //   "& .MuiSvgIcon-root": {
+  //     // Change icon color here
+  //     color: "white",
+  //   },
+  // };
 
   return (
     <div>
@@ -32,65 +129,11 @@ const StyledDatePicker: React.FC<StyledDatePickerProps> = ({
           showDaysOutsideCurrentMonth
           slotProps={{
             textField: {
-              sx: {
-                "& .MuiInputLabel-root": {
-                  color: "white", // Change label color here
-                },
-                "& .MuiInputBase-input": {
-                  color: "white", // Change text color here
-                },
-                "& .MuiOutlinedInput-root": {
-                  "& fieldset": {
-                    borderColor: "white", // Change border color here
-                  },
-                  "&:hover fieldset": {
-                    borderColor: "white", // Change border color on hover here
-                  },
-                  "&.Mui-focused fieldset": {
-                    borderColor: "white", // Change border color when focused here
-                  },
-                },
-                "& .MuiSvgIcon-root": {
-                  // Change icon color here
-                  color: "white",
-                },
-              },
+              sx: textFieldSx,
             },
             popper: {
-              sx: {
-                "& .MuiPaper-root": {
-                  backgroundColor: "#111", // Change calendar background color here
-                  border: "1px solid white", // Change calendar border color here
-                  color: "white", // Change calendar text color here
-                  marginTop: "8px",
-                  marginBottom: "8px",
-                },
-                "& .MuiPickersDay-root": {
-                  color: "white", // Change day text color here
-                },
-                "& .MuiPickersDay-today": {
-                  borderColor: "white", // Change today's date border color here
-                },
-                "& .MuiPickersDay-root.Mui-selected": {
-                  backgroundColor: selectedColor, // Change selected day background color here
-                  color: "white", // Ensure text color is readable
-                },
-                "& .MuiPickersDay-root.MuiPickersDay-dayOutsideMonth": {
-                  color: "rgba(255, 255, 255, 0.5)", // Change days outside current month color here
-                },
-                "& .Mui-selected": {
-                  backgroundColor: selectedColor, // Change selected month background color here
-                  color: "white", // Ensure text color is readable
-                },
-                "& .MuiSvgIcon-root": {
-                  color: "white", // Change icon color here
-                },
-                "& .MuiTypography-root": {
-                  color: "white", // Change text color here
-                },
-              },
+              sx: popperSx,
             },
-            layout: {},
           }}
         />
       </LocalizationProvider>
